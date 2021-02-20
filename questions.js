46aed2df4b9b152c289bd324a940d530646150a3
rthjj <amy.skull14@gmail.com>
2021-02-20 09:56:22 +0100

    first

---
+++ questions.js
@@ -0,0 +1,12 @@
+var questions = [
+  ["Que zappé Sabrina quand elle est stressé ?", "des pommes", " Des pommes de pin", "Elle ne l'est jamais","Des ananas", "4"],
+  ["Dans quelle saison se trouve Sabrina dans une école militaire ?", " Saison 1", " Saison 2", "  Saison 3", "Jamais","2"],
+  ["Quel est le thème de la soirée Halloween organiser par tente bioula ?", "Hôpital", "Asile pour cleptomanes ", "Asile de fou ", "Hôpital psychiatrique","3"],
+  [" Quel est le point commun entre la saison 1 et la saison 3?", "Sabrina a les cheveux courts", " Valérie est la meilleure amie de Sabrina", "Il y a 24 épisodes dans chaque une des saisons ", "L'épisode de la st-valentain est le 16 dans les 2 saison","4"],
+  ["Dans la saison 4 Sabrina est allée habitée à ", "  Paris", " New York", " Adams", "Boston city","1"],
+  [" Pour quel raison Harvey quitte-t-il Sabrina ?", "  Il ne l'aime plus", "  Elle l'a trompé", "  Elle a déménagé", "Il a découvert que c'était une sorcière","4"],
+  ["Quelle est l'adresse de Sabrina jusqu'à la saison 5?", " 13,Conlin road ", "  133,Collin rd ", "133, Collins road", "Je ne sais pas","1"],
+  ["Aaron est un: ", "Metteur en scène et producteur ", " Metteur en scène", " Journaliste", "Producteur et servant","2"],
+  ["quelle est la saison la plus longue ?", " Saison 1", " Saison 7", " Saison 3", " Saison 5","3"],
+  [" comment s'appelle le professeur de biologie de Sabrina dans la saison 1 ?", "w.kraft", "M.larue", "E.poul", " Eugène pool","4"],
+  ];
